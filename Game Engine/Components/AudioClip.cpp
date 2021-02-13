c5bbde9bf9f6e4b115f39f0151c75420b4f198e8
arthur740212 <36757173+arthur740212@users.noreply.github.com>
2021-02-13 19:37:13 +0800

    Audio Clip Crash Fix
    Last commit lacked some functions so it crashes. fixed them

--- Game Engine/Components/AudioClip.cpp
+++ Game Engine/Components/AudioClip.cpp
@@ -5,5 +5,9 @@ AudioClip::AudioClip() : Component(TYPE_BODY)
 	
 }
 
+AudioClip::~AudioClip()
+{}
+
+void AudioClip::Serialize(rapidjson::GenericArray<false, rapidjson::Value>) {}
 
 void AudioClip::Update() {}
