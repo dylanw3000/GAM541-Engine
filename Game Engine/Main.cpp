a904a94f0ecf9fbc0993b0387733bc32c80af0d7
dylanw3000 <dylanw3000@gmail.com>
2021-04-20 13:59:04 -0700

    fullscreen redux

--- Game Engine/Main.cpp
+++ Game Engine/Main.cpp
@@ -1918,7 +1918,7 @@ int main(int argc, char* args[])
 					SDL_WINDOWPOS_UNDEFINED,					// initial y position
 					screenSize[0],										// width, in pixels
 					screenSize[1],										// height, in pixels
-					SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN);
+					SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL | SDL_WINDOW_BORDERLESS);
 			}
 			else {
 				pWindow = SDL_CreateWindow("ConCaveity",		// window title
